cc5d1d4ae6249b7e7ee7d251e7189da31b2b3550
Ravikumar Amireddy <amireddyravi@gmail.com>
2024-11-18 21:42:49 +0530

    expense terraform dev

---
+++ 40-apps/expense.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+dnf install ansible -y
+cd /tmp
+git clone https://github.com/LearnNewTechno/expense-ansible.git
+cd expense-ansible
+ansible-playbook -i inventory.ini mysql.yml
+ansible-playbook -i inventory.ini backend.yml
+ansible-playbook -i inventory.ini frontend.yml
